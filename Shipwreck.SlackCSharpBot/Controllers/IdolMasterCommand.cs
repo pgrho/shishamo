ed0bfb1d37f8aadde557a20c15d96bb1931d2c8f
pgrho <shipwreck@live.jp>
2016-05-26 19:32:10 +0900

    !imascgに-lオプションを追加

--- Shipwreck.SlackCSharpBot/Controllers/IdolMasterCommand.cs
+++ Shipwreck.SlackCSharpBot/Controllers/IdolMasterCommand.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -24,26 +25,70 @@ namespace Shipwreck.SlackCSharpBot.Controllers
         protected override async Task<Message> ExecuteAsyncCore(Message message, string text)
         {
             var tr = text.Trim();
-            var sp = tr.Split(new[] { ' ' }, 2);
+            var sp = tr.Split(new[] { ' ' }, 3);
 
-            var q = sp.FirstOrDefault();
-            var r = sp.ElementAtOrDefault(1) ?? string.Empty;
+            if (sp.FirstOrDefault() == "-l")
+            {
+                var q = sp.ElementAtOrDefault(1) ?? string.Empty;
+                var r = sp.ElementAtOrDefault(2) ?? string.Empty;
 
-            var rarity = r.StartsWith("n", StringComparison.InvariantCultureIgnoreCase) ? "Normal"
+                var rarity = GetRarity(r);
+                var isPlus = IsPlus(r, rarity);
+
+                var l = await GetImageAsync(headline: q, rarity: rarity, isPlus: isPlus);
+
+                if (!l.Any())
+                {
+                    l = await GetImageAsync(kana: q, kanaOperator: "Contains", rarity: rarity, isPlus: isPlus);
+                }
+
+                if (l.Any())
+                {
+                    var sb = new StringBuilder();
+
+                    foreach (var img in l)
+                    {
+                        sb.Append(img.Headline).NewLine();
+                        sb.Append(img.ImageUrl).NewLine();
+                    }
+
+                    return message.CreateReplyMessage(sb.ToString());
+                }
+                else
+                {
+                    return message.CreateReplyMessage("該当する画像が見つかりませんでした。");
+                }
+            }
+            else
+            {
+                var q = sp.FirstOrDefault();
+                var r = sp.ElementAtOrDefault(1) ?? string.Empty;
+
+                var rarity = GetRarity(r);
+                var isPlus = IsPlus(r, rarity);
+
+                var img = await GetRandomImageAsync(q, rarity: rarity, isPlus: isPlus);
+
+                if (img != null)
+                {
+                    return message.CreateReplyMessage($"{img.ImageUrl}#{DateTime.Now.Ticks}");
+                }
+
+                return message.CreateReplyMessage("該当する画像が見つかりませんでした。");
+            }
+        }
+
+        private static string GetRarity(string r)
+        {
+            return r.StartsWith("n", StringComparison.InvariantCultureIgnoreCase) ? "Normal"
                             : r.StartsWith("r", StringComparison.InvariantCultureIgnoreCase) ? "Rare"
                             : r.StartsWith("sr", StringComparison.InvariantCultureIgnoreCase) ? "SRare"
                             : "";
+        }
 
-            var isPlus = rarity == "" ? (bool?)null : r.EndsWith("+");
-
-            var img = await GetRandomImageAsync(q, rarity: rarity, isPlus: isPlus);
-
-            if (img != null)
-            {
-                return message.CreateReplyMessage($"{img.ImageUrl}#{DateTime.Now.Ticks}");
-            }
-
-            return message.CreateReplyMessage("該当する画像が見つかりませんでした。");
+        private static bool? IsPlus(string r, string rarity)
+        {
+            return rarity == "" ? (bool?)null : r.EndsWith("+");
         }
 
         internal async Task<IdolImage> GetRandomImageAsync(string query, string rarity = null, bool? isPlus = null)
@@ -67,16 +112,12 @@ namespace Shipwreck.SlackCSharpBot.Controllers
         {
             var u = $"http://shipwreck.jp/imascg/Image/Search?headline={headline}&headlineOperator={headlineOperator}&kana={kana}&kanaOperator={kanaOperator}&rarity={rarity}&isPlus={isPlus}&count=32";
 
-            var req = (HttpWebRequest)WebRequest.Create(u);
-
-            using (var res = await req.GetResponseAsync())
-            using (var s = res.GetResponseStream())
-            using (var sr = new StreamReader(s, Encoding.UTF8))
+            using (var hc = new HttpClient())
             {
-                var t = await sr.ReadToEndAsync();
-                var r = new JavaScriptSerializer().Deserialize<IdolImageResult>(t);
+                var res = await hc.GetAsync(u);
+                var ir = await res.Content.ReadAsAsync<IdolImageResult>();
 
-                return r.Items;
+                return ir.Items;
             }
         }
     }
